fa8bfa7ee9cb4179d6cef7d6988966ebbcefb41f
Muhammad Shoaib <mshoaib3393@gmail.com>
2025-05-22 03:50:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -748,14 +748,7 @@ def main():
         layout="centered",
         initial_sidebar_state="expanded"
     )
-    st.markdown(
-    """
-    <h2 style='text-align: center; color: #4B8BBE; font-family: "Georgia", serif;'>
-         Created by <strong>Muhammad Shoaib Sattar</strong>
-    </h2>
-    """,
-    unsafe_allow_html=True
-        )
+    
 
     
 
